10c0a820c190308240d9bc279455801c0ebecedf
yan hongbo <yanhb21@chinaunicom.cn>
2021-01-27 14:36:55 +0800

    feat: 封装数据库操作

--- src/main/java/chapter2/service/CustomerService.java
+++ src/main/java/chapter2/service/CustomerService.java
@@ -21,25 +21,6 @@ public class CustomerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
 
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-    static {
-        Properties conf = PropsUtil.loadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver", e);
-        }
-    }
-
 
     /**
      * 获取客户列表
@@ -47,33 +28,8 @@ public class CustomerService {
      * @return
      */
     public List<Customer> getCustomerList() {
-
-        Connection connection = null;
-        try {
-            List<Customer> customerList = new ArrayList<>();
-            String sql = "SELECT * FROM customer";
-            // 获取数据库连接
-            connection = DatabaseHelper.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-            return customerList;
-        } catch (SQLException e) {
-            LOGGER.error("execute sql failure", e);
-        } finally {
-            // 关闭数据库连接
-            DatabaseHelper.closeConnection(connection);
-        }
-        return null;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 
     /**
